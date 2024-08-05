28a6360e6c275841de26230728e34cd6edfb9b6f
gauravpawar 4548 <gauravpawar1470@gmail.com>
2024-08-05 17:38:07 +0530

    program

---
+++ Monday.java
@@ -0,0 +1,158 @@
+
+
+public class Practices {
+    public static void main(String[] args) {
+
+
+        //find the odd number 5
+//            int num =5;
+//            for(int i=0;i<=num;i++)
+//            {
+//                System.out.println(2*i+1);
+//            }
+//
+//            // find the even number
+//            int num =7;
+//            for(int i=0;i<=num;i++) {
+//                System.out.println(2*i);
+//            }
+
+
+
+
+
+            //prime number
+//            int num =12;
+//            int cnt =0;
+//            for(int i=1;i<=num;i++)
+//            {
+//                if(num%i==0)
+//                {
+//                    cnt++;
+//                }
+//
+//            }
+//            if(cnt==2)
+//            {
+//                System.out.println("prime number");
+//            }
+//            else {
+//                System.out.println("not prime number");
+//            }
+
+
+            // factorial number
+//            int num=5;
+//            int fact=1;
+//           for(int i=1;i<=num;i++)
+//          {
+//              fact=fact*i;
+//           }
+//           System.out.println(fact);
+//
+//
+
+           //sum of digit number
+//         int num =354;
+//         int sum =0;
+//         while(num>0)
+//         {
+//             int rem=num%10;
+//             sum=sum+rem;
+//             num=num/10;
+//         }
+//        System.out.println("sum of digit number :"+sum);
+
+
+         // count of number
+//        int num=686485;
+//        int cnt =0;
+//        while(num!=0)
+//        {
+//            cnt++;
+//            num=num/10;
+//
+//        }
+//        System.out.println("count of digit number : "+cnt);
+
+
+              // palindrome number
+//            int num = 1212376;
+//            int temp=num;
+//            int num2=0;
+//            while(num>0)
+//            {
+//                int rem =num%10;
+//                num2=(num2*10)+rem;
+//                num=num/10;
+//
+//            }
+//            if(num2==temp)
+//            {
+//                System.out.println("palindrom number");
+//            }
+//            else {
+//                System.out.println("not palindrom number");
+//            }
+//
+
+
+
+
+          // find the length of last letter
+//
+//        String s = "Hello worldin";
+//        int cnt =0;
+//        for(int i=s.length()-1;i>=0;i--) {
+//            if (s.charAt(i) != ' ') {
+//                cnt++;
+//            } else if (cnt > 0) {
+//                break;
+//            }
+//        }
+//            System.out.print(cnt);
+
+
+         //find the length of first letter
+//        String s2 = "Hello World";
+//        int cnt =0;
+//        for(int i=0;i<s2.length();i++)
+//        {
+//            if(s2.charAt(i)!= ' ')
+//            {
+//                cnt++;
+//            }else if(cnt>0)
+//            {
+//                break;
+//            }
+//
+//        }
+//        System.out.println( "find the length of first letter :"+cnt);
+
+
+
+//        switch(date) {
+
+//           case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Thuesday");
+//                break;
+//            case 3:
+//                System.out.println("wedsday");
+//                break;
+//            case 4:
+//                System.out.println("thursday");
+//                break;
+//            case 5:
+//                System.out.println("friday");
+//                break;
+//            case 6:
+//                System.out.println("saturday");
+//                break;
+//        }
+//        System.out.println("days is the show ");
+
+    }
+}
