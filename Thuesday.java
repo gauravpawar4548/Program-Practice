9275a71316b6d2b8dc8c82a4ef37c0fb217e4366
gauravpawar 4548 <gauravpawar1470@gmail.com>
2024-08-07 07:54:32 +0530

    Pattern Program

---
+++ Thuesday.java
@@ -0,0 +1,184 @@
+
+
+public class Practices {
+    public static void main(String[] args) {
+
+
+
+        //Thuesday
+
+       //Pattern 1
+
+//        for(int i=1;i<=5;i++)                          //*****
+//        {                                              //*****
+//            for(int j=1;j<=5;j++)                      //*****
+//            {                                          //*****
+//                System.out.print("*");                 //*****
+//            }
+//        }
+//        System.out.println();
+
+
+
+   //Pattern 2
+
+//        int start = 5;
+//        int end =1;
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=end;j++)                         //*
+//            {                                               //**
+//                System.out.print("*");                      //***
+//            }                                               //****
+//            System.out.println();                           //*****
+//            end++;
+//        }
+
+
+     
+   //Pattern 3
+
+//
+//        int e =5;
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=e;j++)                           //*****
+//            {                                              //****
+//                System.out.print("*");                     //***
+//            }                                              //**
+//            System.out.println();                          //*
+//            e--;
+//        }
+
+
+       //Pattern 4
+
+//        int S = 9;
+//        int E = 1;
+//        for(int i=1;i<=S;i++)
+//        {
+//            for(int j=1;j<=E;j++)                   //*
+//            {                                       //**
+//                                                    //***
+//                System.out.print("*");              //****
+//            }                                       //*****
+//            System.out.println();                   //****
+//            if(i<5)                                 //***
+//            {                                       //**
+//                E++;                                //*
+//            }
+//            else {
+//                E--;
+//            }
+//        }
+
+
+        //
+
+
+        //
+
+//        int start = 5;
+//        int startcnt = 4;
+//          for(int i=1;i<=5;i++)
+//          {
+//              for(int j=1;j<=start;j++)
+//              {
+//                  if(j<=startcnt)
+//                  {
+//                      System.out.print(" ");
+//                  }
+//                  else {
+//                      System.out.print("*");
+//                  }
+//              }
+//              System.out.println();
+//              start++;
+//              startcnt--;
+//          }
+
+
+                 //Pattern 6
+//              int end = 5;
+//          int space =0;
+//          for(int i=1;i<=13;i++) {
+//              for (int j = 1; j <= end; j++) {
+//                  if (j <= space) {
+//                      System.out.print(" ");
+//                  } else {
+//                      System.out.print("*");
+//                  }
+//              }
+//              System.out.println();
+//              if (i < 5) {
+//                  space++;
+//                  end++;
+//              } else {
+//                  end--;
+//                  space--;
+//              }
+//          }
+//
+
+         //Pattern 7
+//          int end = 5;
+//          for(int i=1;i<=9;i++)
+//          {
+//              for(int j=1;j<=end;j++)
+//              {
+//                  System.out.print("*");
+//              }
+//              System.out.println();
+//              if(i<5)
+//                  end--;
+//              else
+//                  end++;
+//          }
+
+
+        //Pattern
+
+//         int last = 9;
+//         int space =0;
+//         for(int i=1;i<=9;i++)
+//         {
+//             for(int j=1;j<=last;j++)
+//             {
+//                 if(j<=space) {
+//                     System.out.print(" ");
+//                 }else {
+//                     System.out.print("*");
+//                 }
+//             }
+//             System.out.println();
+//             space++;
+//             last--;
+//         }
+
+
+//        //Pattern
+//
+//          int start =9;
+//          int space =4;
+//          for(int i=1;i<=5;i++)
+//          {
+//              for(int j=1;j<=start;j++)
+//              {
+//                  if(j<=space)
+//                  {
+//                      System.out.print(" ");
+//                  }
+//                  else {
+//                      System.out.print("*");
+//                  }
+//              }
+//              System.out.println();
+//              space--;
+//              start--;
+//
+//          }
+
+       
+
+    }
+}
