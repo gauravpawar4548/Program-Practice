7b9755d6eb4ebd2e318bf0ff6b59908dee734acb
gauravpawar 4548 <gauravpawar1470@gmail.com>
2024-08-13 08:40:49 +0530

    String Program

---
+++ Monday12Aug.java
@@ -0,0 +1,118 @@
+ 
+
+
+public class Practices {
+    public static void main(String[] args) {
+
+
+//String basic program in java
+
+        //concate String
+//        String s1="gaurav";
+//        String s2="pawar";
+//
+//        System.out.println(s1.concat(s2));
+//
+
+        //index and char all String
+
+//        String s1="gaurav";
+//        for(int i=0;i<s1.length();i++) {
+//            char gp = s1.charAt(i);
+//            System.out.println("print length and char" +i+" "+gp );
+//        }
+
+        //twise string
+
+//        String s="welcome";
+//        for(int i=0;i<s.length();i++)
+//        {
+//            String s1="";
+//            char s2 = s.charAt(i);
+//            s1=s1+s2+s2;
+//            System.out.print(s1);
+//        }
+
+
+        //Remove a char from given String
+
+//        String s1="Abcdef";
+//        char ch = 'b';
+//        for(int i=0;i<s1.length();i++)
+//        {
+//            if(s1.charAt(i)!=ch)
+//            {
+//                System.out.print(s1.charAt(i));
+//            }
+//        }
+
+        //create space for character
+
+//        String s1 = "abcdef";
+//        for(int i=0;i<s1.length();i++)
+//        {
+//            char ch = s1.charAt(i);
+//            System.out.print(ch+" ");
+//        }
+
+
+        //Sum of digit in string
+//        String s1= "a2b3c4d5";
+//
+//        int sum =0;
+//        for(int i=0;i<s1.length();i++)
+//        {
+//
+//            char ch = s1.charAt(i);
+//            if (ch > '0' && ch < '9')
+//                sum = sum + ch - 48;
+//        }
+//        System.out.print(sum);
+
+
+        //Replace character in String
+
+//        String s1= "virut";
+//        char c1='u';
+//        char c2='a';
+//
+//        String s="";
+//        for(int i=0;i<s1.length();i++)
+//        {
+//            if(s1.charAt(i)==c1)
+//            {
+//                s=s+c2;
+//            }
+//            else {
+//                s=s+s1.charAt(i);
+//            }
+//
+//        }
+//        System.out.print(s);
+//
+
+         //Remove duplicate character in String
+//
+//               String s1="aabsdesfgh";
+//               String s="";
+//               for(int i=0;i<s1.length();i++)
+//               {
+//                   int cnt =0;
+//
+//                   for(int j=0;j<i;j++)
+//                   {
+//                       if(s1.charAt(i)==s1.charAt(j))
+//                       {
+//                           cnt++;
+//                       }
+//
+//                   }
+//                   if(cnt==0)
+//                   {
+//                       System.out.print(s1.charAt(i));
+//                   }
+//
+//               }
+
+
+    }}
